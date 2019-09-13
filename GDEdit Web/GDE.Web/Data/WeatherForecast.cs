cf42c855f423bdc30b1397babf0fbb9e1e308b2b
Alten <altenyt@gmail.com>
2019-09-12 23:56:44 -0400

    Generalize code

--- GDEdit Web/GDE.Web/Data/WeatherForecast.cs
+++ GDEdit Web/GDE.Web/Data/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace GDE.Web.Data
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(9 / 5 * TemperatureC);
 
         public string Summary { get; set; }
     }
