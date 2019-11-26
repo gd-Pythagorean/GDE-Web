d231569cf7675f6d53fb7a582b26ec2c5e54f845
Alten <altenyt@gmail.com>
2019-11-25 20:15:05 -0500

    implement requested changes

---
+++ GDEdit Web/GDE.Web/Pages/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using GDE.Web.Data;
+
+namespace GDE.Web.Pages
+{
+    public static class Helper
+    {
+        public static int SectionToHueMap(GlobalVariables.LinkItems section)
+        {
+            var SectionMapping = new Dictionary<GlobalVariables.LinkItems, int>
+            {
+                {
+                    GlobalVariables.LinkItems.Home,
+                    255
+                },
+                {
+                    GlobalVariables.LinkItems.Levels,
+                    200
+                },
+                {
+                    GlobalVariables.LinkItems.Community,
+                    150
+                },
+                {
+                    GlobalVariables.LinkItems.Help,
+                    20
+                }
+            };
+
+            return SectionMapping[section];
+        }
+    }
+}
