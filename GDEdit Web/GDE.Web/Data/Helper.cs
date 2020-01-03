2cf75cf66b368bc61f0b2490a4650c95742d02ee
Alten <altenyt@gmail.com>
2020-01-02 21:20:36 -0500

    move switch statement inline with function

--- GDEdit Web/GDE.Web/Data/Helper.cs
+++ GDEdit Web/GDE.Web/Data/Helper.cs
@@ -3,12 +3,12 @@
     public static class Helper
     {
         public static int SectionToHueMap(LinkItems section) => section switch
-            {
-                LinkItems.Home => 255,
-                LinkItems.Levels => 200,
-                LinkItems.Community => 150,
-                LinkItems.Help => 35,
-                _ => 255
-            };
+        {
+            LinkItems.Home => 255,
+            LinkItems.Levels => 200,
+            LinkItems.Community => 150,
+            LinkItems.Help => 35,
+            _ => 255
+        };
     }
 }
