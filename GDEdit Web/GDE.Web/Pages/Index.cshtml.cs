90f36b60afa8e0ee03f4c329c31c10016de6e5eb
Alten <altenyt@gmail.com>
2020-01-02 21:19:29 -0500

    remove logger service

--- GDEdit Web/GDE.Web/Pages/Index.cshtml.cs
+++ GDEdit Web/GDE.Web/Pages/Index.cshtml.cs
@@ -1,18 +1,10 @@
 ﻿using GDE.Web.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace GDE.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            this.logger = logger;
-        }
-
         public void OnGet()
         {
             GlobalVariables.CurrentSection = LinkItems.Home;
